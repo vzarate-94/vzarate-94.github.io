b010f756bcd7edfd7b6191e6864582eaaab23d2d
vzarate26 <vzarate26@gmail.com>
2021-11-22 14:31:43 -0500

    fix more mobil responsiveness issues

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ window.addEventListener('scroll', function () {
 
 window.addEventListener('scroll', function () {
   let header = this.document.querySelector('.centered')
-  let windowPosition = window.scrollY > 115
+  let windowPosition = window.scrollY > 100
   header.classList.toggle('scrolling-active2', windowPosition)
 })
 
