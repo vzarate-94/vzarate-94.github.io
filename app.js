658f5de14e8d1afc5d3ab96c43def6d2362ef984
vzarate26 <vzarate26@gmail.com>
2022-01-22 23:13:59 -0500

    add more fun animations to portfolio

--- app.js
+++ app.js
@@ -13,10 +13,12 @@ window.addEventListener('scroll', function () {
 const myPhoto = document.querySelector('#header-container')
 const navBar = document.querySelector('#nav-bar')
 const nameTitle = document.querySelector("#centered")
+const portfolio = document.querySelector("#port-title")
 const t1 = new TimelineMax();
 
 
 t1.fromTo(myPhoto, 1, {height: "200px"}, {height: "600px", ease: Power2.easeInOut })
 
-t1.fromTo(navBar, 1, {height: "200px"}, {height: "6px", ease: Power2.easeInOut })
-t1.fromTo(nameTitle, 1, {height: "60px"}, {height: "200px", ease: Power2.easeInOut })
+t1.fromTo(navBar, 1, {height: "200px"}, {height: "20px", ease: Power2.easeInOut })
+t1.fromTo(nameTitle, 1, {height: "60px"}, {height: "100px", ease: Power2.easeInOut })
+t1.fromTo(portfolio, 1, {height: "40px"}, {height: "70px", ease: Power2.easeInOut })
